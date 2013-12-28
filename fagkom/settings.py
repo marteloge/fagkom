463a4bc278e8ee724984cb177718e77bc56f28cf
Martin Hallén <marthall@outlook.com>
2013-12-28 18:07:51 +0100

    Made path to database relative to the project

--- fagkom/settings.py
+++ fagkom/settings.py
@@ -7,17 +7,20 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/marte/GitHub/fagkom/test.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -122,7 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
