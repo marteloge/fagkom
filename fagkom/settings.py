510ac3969248715c5dda058eea54940251e4abd9
Martin Hallén <marthall@outlook.com>
2013-12-28 18:17:18 +0100

    Actually added alkoformel app to project

--- fagkom/settings.py
+++ fagkom/settings.py
@@ -124,10 +124,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'apps.alkoformel',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
